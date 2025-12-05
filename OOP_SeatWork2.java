72abb8c540ecb65d572cf043d0bea4bc213e28b5
Nicia E. Casaljay <necasaljay@headoffice.corp.bpi.com.ph>
2025-12-05 13:27:41 +0800

    Adding SeatWork2.java and WhiteBoard.java in M2_Activity2

---
+++ OOP_SeatWork2.java
@@ -0,0 +1,12 @@
+package OOP_Act2;
+
+public class OOP_SeatWork2 {
+
+	public static void main(String[] args) {
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumnbers(5, 3);
+		
+		System.out.println("The total is " + String.valueOf(sum));
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,7 @@
+package OOP_Act2;
+
+public class WhiteBoard {
+	int addNumnbers(int addend1, int addend2) {
+		return addend1 + addend2;
+	}
+}
