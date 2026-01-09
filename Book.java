46a67ab34377574ea95929c9ddb36ee932c8dcc0
Niciaaaaa <necasaljay@bpi.com.ph>
2026-01-09 21:20:25 +0800

    add book.java and myapplication.java file

---
+++ Book.java
@@ -0,0 +1,9 @@
+package com.bpi.training.M2Activity1;
+
+public class Book {
+		String title;
+		
+		public void read() {
+			System.out.println("Title: "+ title);
+		}
+}
---
+++ MyApplication.java
@@ -0,0 +1,14 @@
+package com.bpi.training.M2Activity1;
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book mybook = new Book();
+		mybook.title = "Harry Potter";
+		mybook.read();
+		
+		System.out.println();
+	}
+
+}
