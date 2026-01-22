31e76e835fe085af4cad7b5da7089efc985a99dc
Niciaaaaa <necasaljay@bpi.com.ph>
2026-01-22 14:50:35 +0800

    Delete InsufficientFundsExceptions.java

--- InsufficientFundsExceptions.java
+++
@@ -1,7 +0,0 @@
-package com.bpi.training.M4Activity6;
-
-public class InsufficientFundsExceptions extends Exception {
-	public InsufficientFundsExceptions(String message) {
-		super(message);
-	}
-}
